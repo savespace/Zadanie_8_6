fb6c920d5206ceb36c0216866b64e307fc10438d
savespace <savespace@outlook.com>
2017-07-27 12:35:05 +0200

    skrypt-js

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+var a = 5;
+var b = 3;
+var value = (a * a) + (2 * a * b) - (b * b);
+console.log(value);
+if ( value == 0 ) {
+	console.log("Wartość jest równa 0");
+}
+else {
+	console.log("wartość jest większa lub mniejsza od 0");
+	}
